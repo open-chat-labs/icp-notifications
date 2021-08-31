a1e105fe42475bad635edbf6ef7f41437c71daab
Hamish Peebles <hamishpeebles@gmail.com>
2021-08-31 10:45:36 +0100

    Move 'ingest_transaction' into its own mod

--- backend/canisters/notifications/impl/src/lifecycle/heartbeat.rs
+++ backend/canisters/notifications/impl/src/lifecycle/heartbeat.rs
@@ -9,114 +9,123 @@ const MAX_TRANSACTIONS_PER_BATCH: usize = 1000;
 
 #[heartbeat]
 async fn heartbeat() {
-    if let PrepareResult::Ready(ledger, maybe_synced_up_to) =
-        RUNTIME_STATE.with(|state| prepare(state.borrow_mut().as_mut().unwrap()))
-    {
-        if let Some(synced_up_to) = maybe_synced_up_to {
-            let transactions = get_transactions(ledger, synced_up_to + 1)
-                .await
-                .expect("Failed to get transactions");
+    ingest_transactions::run().await;
+}
 
-            RUNTIME_STATE.with(|state| {
-                process_transactions(transactions, state.borrow_mut().as_mut().unwrap())
-            });
+mod ingest_transactions {
+    use super::*;
+
+    pub async fn run() {
+        if let PrepareResult::Ready(ledger, maybe_synced_up_to) =
+            RUNTIME_STATE.with(|state| prepare(state.borrow_mut().as_mut().unwrap()))
+        {
+            if let Some(synced_up_to) = maybe_synced_up_to {
+                let transactions = get_transactions(ledger, synced_up_to + 1)
+                    .await
+                    .expect("Failed to get transactions");
+
+                RUNTIME_STATE.with(|state| {
+                    process_transactions(transactions, state.borrow_mut().as_mut().unwrap())
+                });
+            } else {
+                // This will only happen on the first iteration. We set synced_up_to to be the current
+                // tip_of_chain so that subsequent processing continues from there since we don't care
+                // about transactions prior to this.
+                let tip_of_chain = ledger
+                    .tip_of_chain()
+                    .await
+                    .expect("Failed to call 'tip_of_chain'")
+                    .tip_index;
+
+                RUNTIME_STATE.with(|state| {
+                    set_synced_up_to(tip_of_chain, state.borrow_mut().as_mut().unwrap())
+                });
+            }
+        }
+    }
+
+    enum PrepareResult {
+        AlreadyInProgress,
+        Ready(Arc<dyn Ledger>, Option<u64>),
+    }
+
+    fn prepare(runtime_state: &mut RuntimeState) -> PrepareResult {
+        if runtime_state
+            .data
+            .transaction_import_state
+            .try_start_import()
+        {
+            let ledger = runtime_state.ledger.clone();
+            PrepareResult::Ready(
+                ledger,
+                runtime_state.data.transaction_import_state.synced_up_to(),
+            )
         } else {
-            // This will only happen on the first iteration. We set synced_up_to to be the current
-            // tip_of_chain so that subsequent processing continues from there since we don't care
-            // about transactions prior to this.
-            let tip_of_chain = ledger
-                .tip_of_chain()
-                .await
-                .expect("Failed to call 'tip_of_chain'")
-                .tip_index;
-
-            RUNTIME_STATE
-                .with(|state| set_synced_up_to(tip_of_chain, state.borrow_mut().as_mut().unwrap()));
+            PrepareResult::AlreadyInProgress
         }
     }
-}
 
-enum PrepareResult {
-    AlreadyInProgress,
-    Ready(Arc<dyn Ledger>, Option<u64>),
-}
-
-fn prepare(runtime_state: &mut RuntimeState) -> PrepareResult {
-    if runtime_state
-        .data
-        .transaction_import_state
-        .try_start_import()
-    {
-        let ledger = runtime_state.ledger.clone();
-        PrepareResult::Ready(
-            ledger,
-            runtime_state.data.transaction_import_state.synced_up_to(),
-        )
-    } else {
-        PrepareResult::AlreadyInProgress
-    }
-}
-
-fn set_synced_up_to(synced_up_to: u64, runtime_state: &mut RuntimeState) {
-    runtime_state
-        .data
-        .transaction_import_state
-        .set_synced_up_to(synced_up_to);
-    runtime_state
-        .data
-        .transaction_import_state
-        .mark_import_finished();
-}
-
-async fn get_transactions(
-    ledger: Arc<dyn Ledger>,
-    from: BlockHeight,
-) -> Result<Vec<(BlockHeight, Transaction)>, String> {
-    let blocks = ledger
-        .get_blocks_since(from, MAX_TRANSACTIONS_PER_BATCH)
-        .await?
-        .0?;
-
-    let results: Vec<_> = blocks
-        .into_iter()
-        .enumerate()
-        .map(|(index, block)| (from + (index as u64), block.decode().unwrap().transaction))
-        .collect();
-
-    Ok(results)
-}
-
-fn process_transactions(
-    transactions: Vec<(BlockHeight, Transaction)>,
-    runtime_state: &mut RuntimeState,
-) {
-    for (block_height, transaction) in transactions.into_iter() {
-        let recipient = match &transaction.transfer {
-            Mint { to, amount: _ } => to,
-            Send {
-                from: _,
-                to,
-                amount: _,
-                fee: _,
-            } => to,
-            _ => continue,
-        };
-        let subscriptions = runtime_state.data.subscriptions.get_by_account(*recipient);
-        if subscriptions.is_empty() {
-            continue;
-        }
-        let targets = subscriptions
-            .into_iter()
-            .map(|s| s.targets)
-            .flatten()
-            .collect();
+    fn set_synced_up_to(synced_up_to: u64, runtime_state: &mut RuntimeState) {
         runtime_state
             .data
-            .notifications
-            .add(block_height, transaction, targets);
+            .transaction_import_state
+            .set_synced_up_to(synced_up_to);
+        runtime_state
+            .data
+            .transaction_import_state
+            .mark_import_finished();
     }
-    runtime_state
-        .data
-        .transaction_import_state
-        .mark_import_finished();
+
+    async fn get_transactions(
+        ledger: Arc<dyn Ledger>,
+        from: BlockHeight,
+    ) -> Result<Vec<(BlockHeight, Transaction)>, String> {
+        let blocks = ledger
+            .get_blocks_since(from, MAX_TRANSACTIONS_PER_BATCH)
+            .await?
+            .0?;
+
+        let results: Vec<_> = blocks
+            .into_iter()
+            .enumerate()
+            .map(|(index, block)| (from + (index as u64), block.decode().unwrap().transaction))
+            .collect();
+
+        Ok(results)
+    }
+
+    fn process_transactions(
+        transactions: Vec<(BlockHeight, Transaction)>,
+        runtime_state: &mut RuntimeState,
+    ) {
+        for (block_height, transaction) in transactions.into_iter() {
+            let recipient = match &transaction.transfer {
+                Mint { to, amount: _ } => to,
+                Send {
+                    from: _,
+                    to,
+                    amount: _,
+                    fee: _,
+                } => to,
+                _ => continue,
+            };
+            let subscriptions = runtime_state.data.subscriptions.get_by_account(*recipient);
+            if subscriptions.is_empty() {
+                continue;
+            }
+            let targets = subscriptions
+                .into_iter()
+                .map(|s| s.targets)
+                .flatten()
+                .collect();
+            runtime_state
+                .data
+                .notifications
+                .add(block_height, transaction, targets);
+        }
+        runtime_state
+            .data
+            .transaction_import_state
+            .mark_import_finished();
+    }
 }
