6e7d9fd0638e96d2dce3e9af135f549a7ca51516
Hamish Peebles <hamishpeebles@gmail.com>
2021-08-31 10:11:27 +0100

    Switch to 'get_blocks_since' (which doesn't exist yet)

--- backend/canisters/notifications/impl/src/ledger.rs
+++ backend/canisters/notifications/impl/src/ledger.rs
@@ -1,15 +1,12 @@
 use async_trait::async_trait;
-use ic_base_types::CanisterId;
-use ledger_canister::protobuf::ArchiveIndexResponse;
 use ledger_canister::{BlockHeight, GetBlocksRes, TipOfChainRes};
 
 #[async_trait]
 pub trait Ledger {
     async fn tip_of_chain(&self) -> Result<TipOfChainRes, String>;
-    async fn archive_index(&self) -> Result<ArchiveIndexResponse, String>;
-    async fn get_blocks(
+
+    async fn get_blocks_since(
         &self,
-        canister_id: CanisterId,
         start: BlockHeight,
         length: usize,
     ) -> Result<GetBlocksRes, String>;
@@ -24,16 +21,7 @@ impl Ledger for LedgerCanister {
         todo!()
     }
 
-    async fn archive_index(&self) -> Result<ArchiveIndexResponse, String> {
-        todo!()
-    }
-
-    async fn get_blocks(
-        &self,
-        _canister_id: CanisterId,
-        _start: u64,
-        _length: usize,
-    ) -> Result<GetBlocksRes, String> {
+    async fn get_blocks_since(&self, _start: u64, _length: usize) -> Result<GetBlocksRes, String> {
         todo!()
     }
 }
--- backend/canisters/notifications/impl/src/lifecycle/heartbeat.rs
+++ backend/canisters/notifications/impl/src/lifecycle/heartbeat.rs
@@ -1,13 +1,8 @@
 use crate::ledger::Ledger;
 use crate::{RuntimeState, RUNTIME_STATE};
-use ic_base_types::CanisterId;
 use ic_cdk_macros::heartbeat;
-use ic_nns_constants::LEDGER_CANISTER_ID;
-use ledger_canister::protobuf::ArchiveIndexEntry;
 use ledger_canister::Transfer::{Mint, Send};
 use ledger_canister::{BlockHeight, Transaction};
-use std::cmp::{max, min};
-use std::ops::RangeInclusive;
 use std::sync::Arc;
 
 const MAX_TRANSACTIONS_PER_BATCH: usize = 1000;
@@ -17,24 +12,24 @@ async fn heartbeat() {
     if let PrepareResult::Ready(ledger, maybe_synced_up_to) =
         RUNTIME_STATE.with(|state| prepare(state.borrow_mut().as_mut().unwrap()))
     {
-        let tip_of_chain = ledger
-            .tip_of_chain()
-            .await
-            .expect("Failed to call 'tip_of_chain'")
-            .tip_index;
         if let Some(synced_up_to) = maybe_synced_up_to {
-            if tip_of_chain > synced_up_to {
-                let transactions = get_transactions(ledger, synced_up_to + 1, tip_of_chain)
-                    .await
-                    .expect("Failed to get transactions");
-                RUNTIME_STATE.with(|state| {
-                    process_transactions(transactions, state.borrow_mut().as_mut().unwrap())
-                });
-            }
+            let transactions = get_transactions(ledger, synced_up_to + 1)
+                .await
+                .expect("Failed to get transactions");
+
+            RUNTIME_STATE.with(|state| {
+                process_transactions(transactions, state.borrow_mut().as_mut().unwrap())
+            });
         } else {
             // This will only happen on the first iteration. We set synced_up_to to be the current
             // tip_of_chain so that subsequent processing continues from there since we don't care
             // about transactions prior to this.
+            let tip_of_chain = ledger
+                .tip_of_chain()
+                .await
+                .expect("Failed to call 'tip_of_chain'")
+                .tip_index;
+
             RUNTIME_STATE
                 .with(|state| set_synced_up_to(tip_of_chain, state.borrow_mut().as_mut().unwrap()));
         }
@@ -76,32 +71,16 @@ fn set_synced_up_to(synced_up_to: u64, runtime_state: &mut RuntimeState) {
 async fn get_transactions(
     ledger: Arc<dyn Ledger>,
     from: BlockHeight,
-    tip_of_chain: BlockHeight,
 ) -> Result<Vec<(BlockHeight, Transaction)>, String> {
-    let archive_index_entries = ledger.archive_index().await?.entries;
-
-    let (canister_id, range) =
-        determine_canister_for_blocks(from, tip_of_chain, archive_index_entries);
-
-    let count = min(
-        (range.end() - range.start() + 1) as usize,
-        MAX_TRANSACTIONS_PER_BATCH,
-    );
-
     let blocks = ledger
-        .get_blocks(canister_id, *range.start(), count)
+        .get_blocks_since(from, MAX_TRANSACTIONS_PER_BATCH)
         .await?
         .0?;
 
     let results: Vec<_> = blocks
         .into_iter()
         .enumerate()
-        .map(|(index, block)| {
-            (
-                range.start() + (index as u64),
-                block.decode().unwrap().transaction,
-            )
-        })
+        .map(|(index, block)| (from + (index as u64), block.decode().unwrap().transaction))
         .collect();
 
     Ok(results)
@@ -140,27 +119,4 @@ fn process_transactions(
         .data
         .transaction_import_state
         .mark_import_finished();
-}
-
-fn determine_canister_for_blocks(
-    from: BlockHeight,
-    tip_of_chain: BlockHeight,
-    archive_index_entries: Vec<ArchiveIndexEntry>,
-) -> (CanisterId, RangeInclusive<BlockHeight>) {
-    for archive_index_entry in archive_index_entries.into_iter().rev() {
-        if archive_index_entry.height_to < from {
-            break;
-        } else if archive_index_entry.height_from > from {
-            continue;
-        } else {
-            let range_start = max(from, archive_index_entry.height_from);
-            let range_end = min(tip_of_chain, archive_index_entry.height_to);
-            return (
-                CanisterId::new(archive_index_entry.canister_id.unwrap()).unwrap(),
-                range_start..=range_end,
-            );
-        }
-    }
-
-    (LEDGER_CANISTER_ID, from..=tip_of_chain)
 }
